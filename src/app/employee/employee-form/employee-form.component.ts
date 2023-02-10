671924a4be924dbe10b87f009b1d44b02768ce90
Nikunj Patel <nikunj.patel@1rivet.local>
2023-02-10 20:16:46 +0530

    form started

--- src/app/employee/employee-form/employee-form.component.ts
+++ src/app/employee/employee-form/employee-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-form',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-form.component.scss']
 })
 export class EmployeeFormComponent {
+public employeeForm : FormGroup
 
+constructor(public formBuilder: FormBuilder){
+
+  this.employeeForm = this.formBuilder.group({
+    name:['' ,Validators.required],
+    address:['', Validators.required],
+    mobileNo :['' , Validators.required],
+    gender:['' , Validators.required],
+    salary:['' , Validators.required],
+    birthDate:['' , Validators.required]
+  })
+
+}
+
+// to save employee Data
+public getEmployeeData(){
+ console.log( this.employeeForm.value);
+ this.employeeForm.reset()
+}
 }
