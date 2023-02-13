68dbd666e9bec6bbb0b99aa3e2e2c13dd327d9a0
Nikunj Patel <nikunj.patel@1rivet.local>
2023-02-13 15:44:39 +0530

    feat:employee-form

--- src/app/employee/employee-form/employee-form.component.ts
+++ src/app/employee/employee-form/employee-form.component.ts
@@ -8,23 +8,29 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class EmployeeFormComponent {
 public employeeForm : FormGroup
-
+public isSubmitted:boolean 
 constructor(public formBuilder: FormBuilder){
-
+this.isSubmitted = false;
   this.employeeForm = this.formBuilder.group({
-    name:['' ,Validators.required],
-    address:['', Validators.required],
-    mobileNo :['' , Validators.required],
-    gender:['' , Validators.required],
-    salary:['' , Validators.required],
-    birthDate:['' , Validators.required]
+    name:['' ,[Validators.required ,Validators.pattern('[a-zA-Z]*'), Validators.minLength(3)]],
+    address:['', [Validators.required]],
+    mobileNo :['' , [Validators.required,Validators.pattern('[0-9]*'), Validators.minLength(5)]],
+    gender:['' , [Validators.required]],
+    salary:['' , [Validators.required , Validators.pattern('[0-9]*')]],
+    birthDate:['' , [Validators.required]]
   })
 
 }
 
+get employeeFormControl() {
+ return this.employeeForm.controls
+}
 // to save employee Data
 public getEmployeeData(){
+  this.isSubmitted = true
  console.log( this.employeeForm.value);
  this.employeeForm.reset()
 }
+
+
 }
