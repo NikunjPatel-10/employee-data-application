77b730ce3f07983956d228685ef4c546b000a29a
Nikunj Patel <nikunj.patel@1rivet.local>
2023-02-21 13:10:56 +0530

    some changes

--- src/app/shared/pipe/sort.pipe.ts
+++ src/app/shared/pipe/sort.pipe.ts
@@ -5,26 +5,13 @@ export type SortOrder = 'asc' | 'desc';
   name: 'sort'
 })
 export class SortPipe implements PipeTransform {
-  //   value.sort((a: any, b: any) => {
-  //     if (a < b) {
-  //       return -1
-  //     }
-  //     else if (a > b) {
-  //       return 1
-  //     }
-  //     else {
-  //       return 0
-  //     }
-  //   });
 
-  //   return value
-  // }
   transform(array: any, name: string): any {
-    
-    if (name === 'dsec') {
 
+
+    if (name === 'dsec') {
       array.sort((a: any, b: any) => {
-        if(a.name < b.name) {
+        if (a.name < b.name) {
           return 1;
         } else if (a.name > b.name) {
           return -1;
@@ -33,24 +20,35 @@ export class SortPipe implements PipeTransform {
         }
       });
       return array;
-    } else if(name === 'asec') {
+    }
+
+    else if (name === 'asec') {
       array.sort((a: any, b: any) => {
         if (a.name < b.name) {
           return -1;
         } else if (a.name > b.name) {
           return 1;
         }
-        else{
+        else {
           return 0;
         }
       });
       return array;
-    } 
-    else{
-      return array;
     }
+
+    else {
+        return array;
+    }
+
+
   }
 
+
 }
+
+
+
+
+
 
 
